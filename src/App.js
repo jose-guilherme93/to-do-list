120b280bb392ff9c7b80edf946bcbdd3e2249062
jose-guilherme93 <jose-guilherme93@hotmail.com>
2021-09-08 16:07:41 -0300

    added components Task, AddTask, Tasks

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import './App.css'
-
-const App = () => {
-  
-  return <div className="container"></div>
-};
-
-export default App;
---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+
+import Tasks from './components/Tasks';
+import './App.css'
+import AddTask from "./components/AddTask";
+
+const App = () => {
+  const [tasks, setTasks] = useState ([
+    {
+      id: '1',
+      title: 'Estudar Programação',
+      completed: false,
+
+    },
+    {
+      id: '2',
+      title: 'ler livro',
+      completed: true,
+
+    },
+    {
+      id: '2',
+      title: 'ler livro',
+      completed: true,
+
+    },{
+      id: '2',
+      title: 'ler livro',
+      completed: true,
+
+    },
+  ]);
+    return (
+      <>
+        <div className="container" >
+          <AddTask />
+          <Tasks tasks= {tasks} />
+        </div>     
+      </>
+      );
+};
+
+
+export default App;
